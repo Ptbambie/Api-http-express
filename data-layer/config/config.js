db2c1e565adeaa9f075f4b8007e981551f15d7f8
Ptbambie <audreyalaime3@gmail.com>
2024-01-16 16:46:38 +0100

    Connect to database, serve items

--- data-layer/config/config.js
+++ data-layer/config/config.js
@@ -1,5 +1,9 @@
 const config = {
-  use_env_variable: 'DATABASE_URL',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
   seederStorage: 'sequelize', // Keep track of executed seeds to avoid re-execution upon each deployment
   dialectOptions: {
     ssl:
